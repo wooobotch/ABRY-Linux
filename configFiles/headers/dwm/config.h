68b91569c91e3a1716de4ce163b6989d24adbe78
Abraham Ezequiel Chalave <abraham.chalave@outlook.com>
2025-07-10 01:22:01 -0300

    window management rules for krita and brave

--- configFiles/headers/dwm/config.h
+++ configFiles/headers/dwm/config.h
@@ -27,8 +27,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "krita",     NULL,       NULL,       1 << 2,            0,           -1 },
+	{ "Brave-browser",  NULL,       NULL,    1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
